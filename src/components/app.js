703c6583b8d2fd2a9b79cd92ac675eab1be54cce
丨浅笑安然丨 <49086599+L-20021213@users.noreply.github.com>
2023-03-22 16:40:57 +0800

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -24,7 +24,7 @@ function App() {
         </div>
         <div id='footer'>
           <p>基于 <Link to='https://uptimerobot.com/' text='UptimeRobot' /> 接口制作，检测频率 5 分钟</p>
-          <p>&copy; <script>document.write(new Date().getFullYear())</script> <Link to='https://sianx.com/' text='Blog' /></p>
+          &copy; <script>document.write(new Date().getFullYear())</script>  <p><Link to='https://sianx.com/' text='Blog' /></p>
         </div>
       </div>
     </>
